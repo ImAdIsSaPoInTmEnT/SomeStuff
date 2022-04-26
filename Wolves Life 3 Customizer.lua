68d3da4a21de47e509e20ff9347b5433e86c2d2d
ImAdIsSaPoInTmEnT <53959470+ImAdIsSaPoInTmEnT@users.noreply.github.com>
2022-04-25 21:51:49 -0500

    Update Wolves Life 3 Customizer.lua

--- Wolves Life 3 Customizer.lua
+++ Wolves Life 3 Customizer.lua
@@ -1,179 +1,175 @@
 local Customizer = {}
 
-local Keys = {
-    GamepassAccessories = '\230\139\154\230\136\172i\235\156\146(\238\138\155\201\172XD'
-}
-
 local CustomizationSettings = {
-    GamepassAccessoryNames = {'Wings', 'Dragon', 'Ocean'},
-    FluffNames = {'ChestFluff', 'BackFluff', 'EarFluff', 'JawFluff', 'LegFluff', 'TailFluff', 'Fat', 'ChubbyCheeks'},
-    Keys = {
-        GamepassAccessories = '\230\139\154\230\136\172i\235\156\146(\238\138\155\201\172XD'
-    },
-    PrimaryObjects = {"DragonPrimary", "OceanPrimary", "BackFluff", "TailFluff", "LeftWingStart", "LeftWing2", "LeftWing3", "RightWingStart", "RightWing2", "RightWing3", "EyeLid", "Torso", "Tail1", "Tail2", "Tail3", "Tail5", "Tail6", "RightThigh", "RightEar", "EyeLid", "Head", "Hip", "LeftEar", "LeftThigh", "Muzzle", "Neck", "NeckReal"},
-    SecondaryObjects = {"DragonSecondary", "OceanSecondary", "ChubbyCheeks", "Fat", "EarFluff", "JawFluff", "ChestFluff", "LegFluff", "Eyebrow1", "Eyebrow2", "Secondary", "Jaw", "RightShoulder", "RightLowerLeg", "RightLowerArm", "RightLeg", "RightFootPaw", "LeftArm", "LeftArmPaw", "LeftCarpal", "LeftFootPaw", "LeftLeg", "LeftLowerArm", "LeftLowerLeg", "LeftShoulder", "RightArm", "RightArmPaw", "RightCarpal"},
-    AllObjects = {"DragonPrimary", "OceanPrimary", "BackFluff", "TailFluff", "LeftWingStart", "LeftWing2", "LeftWing3", "RightWingStart", "RightWing2", "RightWing3", "EyeLid", "Torso", "Tail1", "Tail2", "Tail3", "Tail5", "Tail6", "RightThigh", "RightEar", "EyeLid", "Head", "Hip", "LeftEar", "LeftThigh", "Muzzle", "Neck", "NeckReal", "DragonSecondary", "OceanSecondary", "ChubbyCheeks", "Fat", "EarFluff", "JawFluff", "ChestFluff", "LegFluff", "Eyebrow1", "Eyebrow2", "Secondary", "Jaw", "RightShoulder", "RightLowerLeg", "RightLowerArm", "RightLeg", "RightFootPaw", "LeftArm", "LeftArmPaw", "LeftCarpal", "LeftFootPaw", "LeftLeg", "LeftLowerArm", "LeftLowerLeg", "LeftShoulder", "RightArm", "RightArmPaw", "RightCarpal", "InsideEars", "Nose", "Pads", "Claws", "DragonClaws", "DragonThird", "Pupils", "Back", "lash"},
-    Materials = {'Plastic', 'Wood', 'Slate', 'Concrete', 'CorrodedMetal', 'Foil', 'Grass', 'Ice', 'Marble', 'Granite', 'Brick', 'Pebble', 'Sand', 'Fabric', 'SmoothPlastic', 'Metal', 'WoodPlanks', 'Cobblestone', 'Neon', 'Glass', 'ForceField'}
+	GamepassAccessoryNames = {'Wings', 'Dragon', 'Ocean'},
+	FluffNames = {'ChestFluff', 'BackFluff', 'EarFluff', 'JawFluff', 'LegFluff', 'TailFluff', 'Fat', 'ChubbyCheeks'},
+	Keys = {
+		GamepassAccessories = '\230\139\154\230\136\172i\235\156\146(\238\138\155\201\172XD'
+	},
+	PrimaryObjects = {"DragonPrimary", "OceanPrimary", "BackFluff", "TailFluff", "LeftWingStart", "LeftWing2", "LeftWing3", "RightWingStart", "RightWing2", "RightWing3", "EyeLid", "Torso", "Tail1", "Tail2", "Tail3", "Tail5", "Tail6", "RightThigh", "RightEar", "EyeLid", "Head", "Hip", "LeftEar", "LeftThigh", "Muzzle", "Neck", "NeckReal"},
+	SecondaryObjects = {"DragonSecondary", "OceanSecondary", "ChubbyCheeks", "Fat", "EarFluff", "JawFluff", "ChestFluff", "LegFluff", "Eyebrow1", "Eyebrow2", "Secondary", "Jaw", "RightShoulder", "RightLowerLeg", "RightLowerArm", "RightLeg", "RightFootPaw", "LeftArm", "LeftArmPaw", "LeftCarpal", "LeftFootPaw", "LeftLeg", "LeftLowerArm", "LeftLowerLeg", "LeftShoulder", "RightArm", "RightArmPaw", "RightCarpal"},
+	AllObjects = {"DragonPrimary", "OceanPrimary", "BackFluff", "TailFluff", "LeftWingStart", "LeftWing2", "LeftWing3", "RightWingStart", "RightWing2", "RightWing3", "EyeLid", "Torso", "Tail1", "Tail2", "Tail3", "Tail5", "Tail6", "RightThigh", "RightEar", "EyeLid", "Head", "Hip", "LeftEar", "LeftThigh", "Muzzle", "Neck", "NeckReal", "DragonSecondary", "OceanSecondary", "ChubbyCheeks", "Fat", "EarFluff", "JawFluff", "ChestFluff", "LegFluff", "Eyebrow1", "Eyebrow2", "Secondary", "Jaw", "RightShoulder", "RightLowerLeg", "RightLowerArm", "RightLeg", "RightFootPaw", "LeftArm", "LeftArmPaw", "LeftCarpal", "LeftFootPaw", "LeftLeg", "LeftLowerArm", "LeftLowerLeg", "LeftShoulder", "RightArm", "RightArmPaw", "RightCarpal", "InsideEars", "Nose", "Pads", "Claws", "DragonClaws", "DragonThird", "Pupils", "Back", "lash"},
+	Materials = {'Plastic', 'Wood', 'Slate', 'Concrete', 'CorrodedMetal', 'Foil', 'Grass', 'Ice', 'Marble', 'Granite', 'Brick', 'Pebble', 'Sand', 'Fabric', 'SmoothPlastic', 'Metal', 'WoodPlanks', 'Cobblestone', 'Neon', 'Glass', 'ForceField'}
 }
 
 function Customizer:SetEyeColor(r, g, b)
-    local _, e = pcall(function()
-        r = tonumber(r)
-        g = tonumber(g)
-        b = tonumber(b)
-    end)
-    if not e then
-        game.ReplicatedStorage.MasterKey:FireServer(
-            'customize',
-            {
-                'EyeColor'
-            },
-            Color3.fromRGB(
-                r,
-                g,
-                b
-            ),
-            'Body'
-        )
+	local _, e = pcall(function()
+		r = tonumber(r)
+		g = tonumber(g)
+		b = tonumber(b)
+	end)
+	if not e then
+		game.ReplicatedStorage.MasterKey:FireServer(
+			'customize',
+			{
+				'EyeColor'
+			},
+			Color3.fromRGB(
+				r,
+				g,
+				b
+			),
+			'Body'
+		)
 
-        return 'success'
-    end
+		return 'success'
+	end
 
-    return 'failed'
+	return 'failed'
 end
 
 function Customizer:SetPrimaryColor(r, g, b)
-    local _, e = pcall(function()
-        r = tonumber(r)
-        g = tonumber(g)
-        b = tonumber(b)
-    end)
-    if not e then
-        game.ReplicatedStorage.MasterKey:FireServer(
-            'customize',
-            CustomizationSettings.PrimaryObjects,
-            Color3.fromRGB(
-                r,
-                g,
-                b
-            ),
-            'Body'
-        )
+	local _, e = pcall(function()
+		r = tonumber(r)
+		g = tonumber(g)
+		b = tonumber(b)
+	end)
+	if not e then
+		game.ReplicatedStorage.MasterKey:FireServer(
+			'customize',
+			CustomizationSettings.PrimaryObjects,
+			Color3.fromRGB(
+				r,
+				g,
+				b
+			),
+			'Body'
+		)
 
-        return 'success'
-    end
+		return 'success'
+	end
 
-    return 'failed'
+	return 'failed'
 end
 
 function Customizer:SetSecondaryColor(r, g, b)
-    local _, e = pcall(function()
-        r = tonumber(r)
-        g = tonumber(g)
-        b = tonumber(b)
-    end)
-    if not e then
-        game.ReplicatedStorage.MasterKey:FireServer(
-            'customize',
-            CustomizationSettings.SecondaryObjects,
-            Color3.fromRGB(
-                r,
-                g,
-                b
-            ),
-            'Body'
-        )
+	local _, e = pcall(function()
+		r = tonumber(r)
+		g = tonumber(g)
+		b = tonumber(b)
+	end)
+	if not e then
+		game.ReplicatedStorage.MasterKey:FireServer(
+			'customize',
+			CustomizationSettings.SecondaryObjects,
+			Color3.fromRGB(
+				r,
+				g,
+				b
+			),
+			'Body'
+		)
 
-        return 'success'
-    end
+		return 'success'
+	end
 
-    return 'failed'
+	return 'failed'
 end
 
 function Customizer:SetMaterial(material, section)
-    if material ~= nil then
-        if table.find(CustomizationSettings.Materials, material) then
-            if section == 'all' then
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    'Material',
-                    material,
-                    CustomizationSettings.AllObjects
-                )
+	if material ~= nil then
+		if table.find(CustomizationSettings.Materials, material) then
+			if section == 'all' then
+				game.ReplicatedStorage.MasterKey:FireServer(
+					'Material',
+					material,
+					CustomizationSettings.AllObjects
+				)
 
-                return 'success'
-            elseif section == 'primary' then
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    'Material',
-                    material,
-                    CustomizationSettings.PrimaryObjects
-                )
+				return 'success'
+			elseif section == 'primary' then
+				game.ReplicatedStorage.MasterKey:FireServer(
+					'Material',
+					material,
+					CustomizationSettings.PrimaryObjects
+				)
 
-                return 'success'
-            elseif section == 'secondary' then
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    'Material',
-                    material,
-                    CustomizationSettings.SecondaryObjects
-                )
+				return 'success'
+			elseif section == 'secondary' then
+				game.ReplicatedStorage.MasterKey:FireServer(
+					'Material',
+					material,
+					CustomizationSettings.SecondaryObjects
+				)
 
-                return 'success'
-            end
-        end
-    end
+				return 'success'
+			end
+		end
+	end
 
-    return 'failed'
+	return 'failed'
 end
 
 function Customizer:ToggleGamepassAccessory(accessoryName, toggle)
-    if accessoryName ~= nil then
-        if table.find(CustomizationSettings.GamepassAccessoryNames) then
-            if not toggle then
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    accessoryName,
-                    1,
-                    Keys.GamepassAccessories
-                )
-                
-                return 'success'
-            else
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    accessoryName,
-                    0,
-                    Keys.GamepassAccessories
-                )
+	if accessoryName ~= nil then
+		if table.find(CustomizationSettings.GamepassAccessoryNames) then
+			if not toggle then
+				game.ReplicatedStorage.MasterKey:FireServer(
+					accessoryName,
+					1,
+					CustomizationSettings.Keys.GamepassAccessories
+				)
 
-                return 'success'
-            end
-        end
-    end
+				return 'success'
+			else
+				game.ReplicatedStorage.MasterKey:FireServer(
+					accessoryName,
+					0,
+					CustomizationSettings.Keys.GamepassAccessories
+				)
 
-    return 'failed'
+				return 'success'
+			end
+		end
+	end
+
+	return 'failed'
 end
 
 function Customizer:ToggleFluff(fluffName, toggle)
-    if fluffName ~= nil then
-        if table.find(CustomizationSettings.FluffNames, fluffName) then
-            if not toggle then
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    'Fluff',
-                    fluffName,
-                    1
-                )
+	if fluffName ~= nil then
+		if table.find(CustomizationSettings.FluffNames, fluffName) then
+			if not toggle then
+				game.ReplicatedStorage.MasterKey:FireServer(
+					'Fluff',
+					fluffName,
+					1
+				)
 
-                return 'success'
-            else
-                game.ReplicatedStorage.MasterKey:FireServer(
-                    'Fluff',
-                    fluffName,
-                    0
-                )
+				return 'success'
+			else
+				game.ReplicatedStorage.MasterKey:FireServer(
+					'Fluff',
+					fluffName,
+					0
+				)
 
-                return 'success'
-            end
-        end
-    end
+				return 'success'
+			end
+		end
+	end
 
-    return 'failed'
+	return 'failed'
 end
 
 return Customizer
